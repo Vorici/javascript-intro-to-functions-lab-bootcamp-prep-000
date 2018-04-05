bafcb1563c7c17dfd23d8f9a1fc151229de12159
Ledio Bode <l.bode515@gmail.com>
2018-04-05 19:20:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -22,9 +22,9 @@ function logWhisper(string){
 
 function sayHiToGrandma(string){
   
-  if (string.toUpperCase() === string) {    return "I can\'t hear you!";
+  if (string.toLowerCase() === string) {    return "I can\'t hear you!";
 
-  } else if (string.toLowerCase() === string) {
+  } else if (string.toUpperCase() === string) {
    return "YES INDEED!";
 
   } else if (string === "I love you, Grandma.") {
